21d10b91357356006b3ea7fa18aaf403d02bbe3c
Reekdeb Mal <reekdeb@gmail.com>
2021-04-09 01:39:30 +0530

    Add ProductManager

---
+++ Assets/Scripts/ProductManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductManager : MonoBehaviour
+{
+    [SerializeField]
+    private TextAsset productCatalogAsset;
+    [SerializeField]
+    private ProductCatalog productCatalog;
+    public ProductCatalog Catalog { get { if (productCatalog != null) return productCatalog; else FetchProducts(); return productCatalog; } }
+
+    private void Awake()
+    {
+        FetchProducts();
+    }
+
+    private void FetchProducts()
+    {
+        productCatalog = JsonUtility.FromJson<ProductCatalog>(productCatalogAsset.text);
+    }
+}

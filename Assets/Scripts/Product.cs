c07b654afa867481cdff5e83f52891991a859501
Reekdeb Mal <reekdeb@gmail.com>
2021-04-09 01:39:03 +0530

    Add Product model

---
+++ Assets/Scripts/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+[Serializable]
+public class ProductCatalog
+{
+    public Product[] products;
+}
+
+[Serializable]
+public class Product
+{
+    public string productName;
+    public string category;
+    public string subCategory;
+}
